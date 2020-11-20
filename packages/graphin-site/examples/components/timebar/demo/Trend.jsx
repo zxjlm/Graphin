cf61d30486447231bf026219e300a1f6379e73bf
kangkui <kangkui.xkk@alibaba-inc.com>
2020-11-21 00:25:18 +0800

    update demo

--- packages/graphin-site/examples/components/timebar/demo/Trend.jsx
+++ packages/graphin-site/examples/components/timebar/demo/Trend.jsx
@@ -18,9 +18,37 @@ const App = () => {
     });
   }
 
+  const onTick = () => {
+    let minx = 99999999;
+    let maxx = -99999999;
+    let miny = 99999999;
+    let maxy = -99999999;
+    const maxsize = -9999999;
+    data.nodes.forEach((node) => {
+      if (minx > node.x) {
+        minx = node.x;
+      }
+      if (maxx < node.x) {
+        maxx = node.x;
+      }
+      if (miny > node.y) {
+        miny = node.y;
+      }
+      if (maxy < node.y) {
+        maxy = node.y;
+      }
+    });
+    const scalex = (constrainBox.width - nodeSize / 2) / (maxx - minx);
+    const scaley = (constrainBox.height - nodeSize / 2) / (maxy - miny);
+    data.nodes.forEach((node) => {
+      node.x = (node.x - minx) * scalex + constrainBox.x;
+      node.y = (node.y - miny) * scaley + constrainBox.y;
+    });
+  };
+
   return (
     <div>
-      <Graphin data={data} layout={{ name: 'force' }}>
+      <Graphin data={data} layout={{ name: 'force', onTick }}>
         <Toolbar />
         <TimeBar timeBarData={timeBarData} />
       </Graphin>
